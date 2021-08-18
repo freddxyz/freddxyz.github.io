54b6acc195000dd0618bed5dd38f70bc7845e0fc
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2021-08-18 00:15:22 -0400

    Added spinning with delta time + red stripe

--- landing/landing.js
+++ landing/landing.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     var canvas = document.getElementById("landing_canvas");
     var context = canvas.getContext("2d")
-    var FRAME_RATE = 1000 / 60; //ms - 1 second / 60 frames = 60 fps
     //
     setup: {
         context.canvas.width = window.innerWidth;
@@ -10,14 +9,25 @@ window.onload = function(){
         
     }
     var ticker = 0;
+    var prevTick = Date.now();
+    var deltaTime = 0;
+    var rot = 0
     var draw = function(){
+        requestAnimationFrame(draw);
         context.canvas.width = window.innerWidth;
         context.canvas.height = window.innerHeight;
-        fillBg("black");
+        let currentTick = Date.now()
+        deltaTime = (currentTick - prevTick) / 1000;
+        prevTick = currentTick;
+
+        if(ticker < 2.125){
+            rot += 360 * deltaTime;
+        }
         context.translate(window.innerWidth/2, window.innerHeight/2)
-        context.rotate(ticker * (180/Math.PI))
+        context.rotate(rot * (Math.PI/180))
         rect(100, 100, "white");
-        ticker+=FRAME_RATE/2;
+        rect(33, 100, "red");
+        ticker += deltaTime;
     }
 
     var fillBg = function(color){
@@ -29,6 +39,5 @@ window.onload = function(){
         context.fillStyle = color;
         context.fillRect(-w/2, -h/2, w, h);
     }
-
-    setInterval(draw, FRAME_RATE);
+    requestAnimationFrame(draw);
 }
