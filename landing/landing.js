d6931007361fe26889e5e6680c9fe961da2e3cc6
FradZGenius <42040138+FradZGenius@users.noreply.github.com>
2021-08-23 01:09:17 -0400

    Added proper centering + finished first part of animation
    code still looks like garbage but oh well

--- landing/landing.js
+++ landing/landing.js
@@ -2,6 +2,7 @@ function cosineInterpolate(a, b, t){
     return a + (((-Math.cos(Math.PI*t))/2) + .5) * (b-a);
 }
 
+
 class Rectangle {
     constructor(w,h, color, context){
         this.size = new vec2(w, h);
@@ -59,7 +60,8 @@ window.onload = function(){
     var prevTick = Date.now();
     var deltaTime = 0;
     var rot = 0
-    var whiteSquare = new Rectangle(4000,4000, "white", context);
+    var initialSize = new vec2(window.innerWidth, window.innerHeight);
+    var whiteSquare = new Rectangle(initialSize.x + initialSize.y,initialSize.x + initialSize.y, "white", context);
     var redStripe = new Rectangle(0, 0, "red", context);
     redStripe.parent = whiteSquare;
 
@@ -80,39 +82,55 @@ window.onload = function(){
         prevTick = currentTick;
         //let minimumDimension = Math.min(window.innerWidth, window.innerHeight);
         context.font = (window.innerWidth / 10) + 'px Roboto';
-        console.log(context.font)
         let sqDiag = Math.sqrt(2 * (Math.pow(whiteSquare.size.x/2, 2)));
         if(ticker < 1){
             whiteSquare.position = screenCenter;
-            whiteSquare.size.x -= deltaTime * (4000 - window.innerWidth/10);
-            whiteSquare.size.y -= deltaTime * (4000 - window.innerWidth/10);
+            whiteSquare.size.x = cosineInterpolate(initialSize.x + initialSize.y, window.innerWidth/10, ticker);
+            whiteSquare.size.y = cosineInterpolate(initialSize.x + initialSize.y, window.innerWidth/10, ticker);
             whiteSquare.rotation += 720 * deltaTime;   
         }else if(ticker > 1 && ticker < 1.5) {
-            squareOffset.x = cosineInterpolate(0, context.measureText("FradZGenius").width/2 + 25, (ticker - 1) / .5)
-            console.log(squareOffset.x)
-            whiteSquare.position = screenCenter.sub(squareOffset);
+            squareOffset.x = cosineInterpolate(0, context.measureText("FradZGenius").width/2 + sqDiag * .125, (ticker - 1) / .5)
             whiteSquare.size.x = window.innerWidth/10;
             whiteSquare.size.y = window.innerWidth/10;
-            whiteSquare.rotation = cosineInterpolate(0, -405, (ticker - 1) / .5);
+            whiteSquare.rotation = cosineInterpolate(0, -495, (ticker - 1) / .5);
             squareX = window.innerWidth/2 - (ticker - 1) * 500;
         }else if(ticker > 1.5 && ticker < 2){
+            squareOffset.x = context.measureText("FradZGenius").width/2 + sqDiag * .125;
             context.fillStyle = 'rgba(255,255,255,' + cosineInterpolate(0, 1, (ticker-1.5)/.5) + ')';
             context.textAlign = "center"
             context.textBaseline = "middle"
-            context.fillText("FradZGenius", window.innerWidth/2 + sqDiag - cosineInterpolate(sqDiag, 0, (ticker-1.5)/.5), window.innerHeight/2);
-            whiteSquare.rotation = 45;
-            redStripe.size.x = whiteSquare.size.x / 3
+            context.fillText("FradZGenius",  window.innerWidth/2 + sqDiag * 1.125 - cosineInterpolate(sqDiag, 0, (ticker-1.5)/.5), window.innerHeight/2);
+            whiteSquare.rotation = -495;
+            whiteSquare.size.x = window.innerWidth/10;
+            whiteSquare.size.y = window.innerWidth/10;
+            redStripe.size.x = whiteSquare.size.x / 3;
             redStripe.size.y = cosineInterpolate(0, whiteSquare.size.y, (ticker-1.5)/.5);
-        } else{
+        } else if(ticker > 2 && ticker < 3){
+            squareOffset.x = context.measureText("FradZGenius").width/2 + sqDiag * .125;
             context.fillStyle = "white";
             context.textAlign = "center";
             context.textBaseline = "middle";
-            context.fillText("FradZGenius", window.innerWidth/2 + sqDiag, window.innerHeight/2);
-            whiteSquare.rotation = 45;
+            context.fillText("FradZGenius", window.innerWidth/2 + sqDiag * 1.125, window.innerHeight/2);///0.625
+            whiteSquare.size.x = window.innerWidth/10;
+            whiteSquare.size.y = window.innerWidth/10;
             redStripe.size.x = whiteSquare.size.x / 3
             redStripe.size.y = whiteSquare.size.y;
-        }
+        } else if(ticker > 3 && ticker < 3.5){
+            context.fillStyle = "white";
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillStyle = 'rgba(255,255,255,' + cosineInterpolate(1, 0, (ticker-3)*2) + ')';
+            context.fillText("FradZGenius", window.innerWidth/2 + sqDiag * 1.125 - cosineInterpolate(0, sqDiag, (ticker-3)/.5), window.innerHeight/2);
+            redStripe.size.y = cosineInterpolate(whiteSquare.size.y, 0, (ticker-3)/.5)
 
+            squareOffset.x = cosineInterpolate(context.measureText("FradZGenius").width/2 + sqDiag * .125, 0, (ticker-3)/.5);
+            whiteSquare.rotation = cosineInterpolate(-495, 45, (ticker-3)/.5)
+        } else if (ticker > 3.5 && ticker < 4){
+            whiteSquare.size.x = cosineInterpolate(window.innerWidth/10, initialSize.x + initialSize.y, (ticker-3.5)/.5);
+            whiteSquare.size.y = cosineInterpolate(window.innerWidth/10, initialSize.x + initialSize.y, (ticker-3.5)/.5);
+            whiteSquare.rotation = cosineInterpolate(45, 200, (ticker - 3.5)/.5)
+        }
+        whiteSquare.position = screenCenter.sub(squareOffset);
         whiteSquare.draw();
         redStripe.draw();
         ticker += deltaTime;
